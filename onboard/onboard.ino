3bbdc0325b185563ba9ee3f8c3b284839fe6de26
Anthony Keba <aclkeba@gmail.com>
2018-10-01 15:14:47 -0700

    added initial Arduino Project for onboard program

---
+++ onboard/onboard.ino
@@ -0,0 +1,15 @@
+// v 1.x of LMU's SAE Aero control system ( aka LMUSAEACS, because fuck it, why not)
+// Code contained represents what will be used onboard the craft itself.
+
+int onboardLEDPin = 13; // representing the onboard LED Pin for most Arduinos, this should be reserved for nothing else.
+int payloadDoorServoDataPin = 9 // representing the pin that should be used for controlling the servo that will actuate the payload bay doors
+
+
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
